987e9baefffe749bcca5b52d22c1406bd353df26
Nilo Batson <djbatson19@gmail.com>
2022-07-20 21:42:52 -0700

    search bar component

--- food/App.js
+++ food/App.js
@@ -1,26 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import SearchScreen from "./src/screens/SearchScreen";
+import { StyleSheet } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
 
-const navigator = createStackNavigator({
-  Search: SearchScreen
-},
-  {
-    initialRouteName: 'Search',
-    defaultNavigationOptions: {
-    title: 'Business Search'
-  }}
-)
+const navigator = createStackNavigator(
+	{
+		Search: SearchScreen,
+	},
+	{
+		initialRouteName: 'Search',
+		defaultNavigationOptions: {
+			title: 'Business Search',
+		},
+	}
+);
 
-export default createAppContainer(navigator)
+export default createAppContainer(navigator);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
